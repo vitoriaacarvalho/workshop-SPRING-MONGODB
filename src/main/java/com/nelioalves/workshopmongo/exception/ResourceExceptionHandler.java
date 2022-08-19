04a3369e8470e114b1ea1ccdc8da011331667465
vitoria <vitoriabccorrea@gmail.com>
2022-08-19 16:10:56 -0300

    FINDBYID() method

---
+++ src/main/java/com/nelioalves/workshopmongo/exception/ResourceExceptionHandler.java
@@ -0,0 +1,5 @@
+package com.nelioalves.workshopmongo.exception;
+
+public class ResourceExceptionHandler {
+
+}
--- src/main/java/com/nelioalves/workshopmongo/services/UserService.java
+++ src/main/java/com/nelioalves/workshopmongo/services/UserService.java
@@ -1,11 +1,13 @@
 package com.nelioalves.workshopmongo.services;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.nelioalves.workshopmongo.domain.User;
+import com.nelioalves.workshopmongo.exception.ObjectNotFoundException;
 import com.nelioalves.workshopmongo.repositories.UserRepository;
 
 @Service
@@ -16,6 +18,11 @@ public class UserService {
 	
 	public List<User> findAll(){
 		return repo.findAll();
+	}
+	
+	public User findById(String id) {
+		Optional<User> obj=repo.findById(id);
+		return obj.orElseThrow(()-> new ObjectNotFoundException("object not found."));
 	}
 	
 }
